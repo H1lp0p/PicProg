c066cb96ed1386e89c7bcd84ed7298a842743b3d
Sonya <sofyakovalenko2005610@gmail.com>
2024-05-24 18:36:56 +0700

    Preparations for cube

--- app/src/main/java/com/example/picprog/CubeActivity.kt
+++ app/src/main/java/com/example/picprog/CubeActivity.kt
@@ -1,4 +1,18 @@
 package com.example.picprog
 
-class CubeActivity {
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.activity.ComponentActivity
+
+class CubeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cube_page)
+
+        val imgView = findViewById<ImageView>(R.id.imgView)
+        val backBtn = findViewById<ImageButton>(R.id.backBtn)
+
+
+    }
 }
